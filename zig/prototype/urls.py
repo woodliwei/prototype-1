2dd56a7e1ca27e890c327965dc6f1b075913f37f
woodli <wood.li@zigstrat.com>
2015-12-29 09:10:26 +0800

    implement trend testing result page
    1. implement trend testing result page
    2. move data from views.py to models.py

--- zig/prototype/urls.py
+++ zig/prototype/urls.py
@@ -7,5 +7,6 @@ app_name = "prototype"
 urlpatterns = [
     url(r'^bigdata/$', views.bigdata_trend_testing, name='bigdata'),
     url(r'^strategy/$', views.strat_trend_manage, name='strategy'),
+    url(r'^bigdata_result/$', views.bigdata_trend_test_result, name='bigdata_result'),
     url(r'^$', views.strat_trend_manage),
 ]
