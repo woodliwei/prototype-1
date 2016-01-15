1d49048ccbd0407e758186a6eaf28e4df2f38ec3
woodli <wood.li@zigstrat.com>
2016-01-15 09:42:59 +0800

    change db connection to sqlite
    change db connection to sqlite

--- zig/zigweb/settings.py
+++ zig/zigweb/settings.py
@@ -75,15 +75,19 @@ WSGI_APPLICATION = 'zigweb.wsgi.application'
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'strats',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '3306'
+      'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+      }
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'strats',
+        # 'USER': 'root',
+        # 'PASSWORD': '123456',
+        # 'HOST': 'localhost',
+        # 'PORT': '3306'
 
-    }
+    # }
 }
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
